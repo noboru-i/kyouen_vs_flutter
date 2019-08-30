7371ff7ffa2c67bf13bc89f9c77a198e15417f3b
noboru-i <ishikura.noboru@gmail.com>
2019-08-30 22:09:43 +0900

    show stone

--- lib/pages/kyouen/kyouen_page.dart
+++ lib/pages/kyouen/kyouen_page.dart
@@ -19,16 +19,75 @@ class KyouenPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Kyouen"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              room.createdAt.toIso8601String(),
-            ),
-          ],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          _KyouenView(
+            room: room,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _KyouenView extends StatelessWidget {
+  final Room room;
+
+  _KyouenView({@required this.room});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.0,
+      child: Container(
+        color: Colors.green,
+        child: GridView.builder(
+          itemCount: 6 * 6,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 6,
+          ),
+          itemBuilder: (context, index) {
+            final state = index % 2 == 0 ? StoneState.Black : StoneState.White;
+            return _StoneView(
+              state: state,
+            );
+          },
         ),
       ),
     );
   }
+}
+
+class _StoneView extends StatelessWidget {
+  final StoneState state;
+
+  _StoneView({
+    this.state = StoneState.None,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (state == StoneState.None) {
+      return Container();
+    }
+
+    final isBlack = state == StoneState.Black;
+    return ClipOval(
+      child: Material(
+        color: isBlack ? Colors.black87 : Colors.white, // button color
+        child: InkWell(
+          splashColor:
+              isBlack ? Colors.white60 : Colors.black45, // inkwell color
+          onTap: () {},
+        ),
+      ),
+    );
+  }
+}
+
+enum StoneState {
+  None,
+  Black,
+  White,
 }
