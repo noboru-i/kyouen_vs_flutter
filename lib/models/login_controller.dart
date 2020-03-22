0caf214ebbb7edb02313593fcf1e1e7b076c67ee
noboru-i <ishikura.noboru@gmail.com>
2020-03-22 14:36:05 +0900

    convert login process to ValueNotifier

---
+++ lib/models/login_controller.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kyouen_vs_flutter/entities/resource.dart';
+import 'package:kyouen_vs_flutter/repositories/login_repository.dart';
+
+part 'login_controller.freezed.dart';
+
+@freezed
+abstract class LoginUser with _$LoginUser {
+  const factory LoginUser({
+    @required String name,
+  }) = _LoginUser;
+}
+
+class LoginController extends ValueNotifier<Resource<LoginUser>> {
+  LoginController(this._loginRepository)
+      : assert(_loginRepository != null),
+        super(const Resource<LoginUser>.loading()) {
+    _initialFetch();
+  }
+
+  final LoginRepository _loginRepository;
+
+  Future<void> _initialFetch() async {
+    final FirebaseUser currentUser = await _loginRepository.currentUser();
+    _updateLoginUserState(currentUser);
+  }
+
+  void _updateLoginUserState(FirebaseUser firebaseUser) {
+    if (firebaseUser == null) {
+      value = const Resource<LoginUser>.error('not logged in');
+      return;
+    }
+    // TODO(noboru-i): we need to add more variables.
+    value = Resource<LoginUser>(LoginUser(name: firebaseUser.uid));
+  }
+
+  Future<void> signInAnonymously() async {
+    final FirebaseUser firebaseUser =
+        await _loginRepository.signInAnonymously();
+    _updateLoginUserState(firebaseUser);
+  }
+}
